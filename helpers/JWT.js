87ba02f456c6979be866dbcd91ee0fbf82ce5acf
jose <15gonzalez.j.p@gmail.com>
2022-06-22 13:32:29 -0400

    update expire time jwt

--- helpers/JWT.js
+++ helpers/JWT.js
@@ -1,10 +1,14 @@
 const jwt = require("jsonwebtoken");
 
+const options = {
+  expiresIn: "365d",
+};
+
 const generateJwt = (uid) => {
   return new Promise((resolve, reject) => {
     const payload = { uid };
 
-    jwt.sign(payload, process.env.KEY, (err, token) => {
+    jwt.sign(payload, process.env.KEY, options, (err, token) => {
       if (err) {
         console.log(err);
         reject("Token no generado");
